72fb2c81821f92e5d2ef0104dae143cb853b3209
SolisAmicus <solisamicus@163.com>
2024-12-10 20:07:35 +0800

    feat(203. 移除链表元素): 完成

---
+++ ljj/src/main/java/leetcode/editor/cn/LinkedLists/P203_RemoveLinkedListElements.java
@@ -0,0 +1,53 @@
+package leetcode.editor.cn.LinkedLists;
+
+/**
+ * 移除链表元素
+ *
+ * @author solisamicus
+ * @date 2024-12-10 19:33:47
+ */
+public class P203_RemoveLinkedListElements {
+
+    public static void main(String[] args) {
+        Solution solution = new P203_RemoveLinkedListElements().new Solution();
+        Solution solution2 = new P203_RemoveLinkedListElements().new Solution();
+
+        ListUtils.printList(solution.removeElements(ListUtils.arrayToList(new int[]{1, 2, 6, 3, 4, 5, 6}), 6));
+        ListUtils.printList(solution.removeElements(ListUtils.arrayToList(new int[]{}), 1));
+        ListUtils.printList(solution.removeElements(ListUtils.arrayToList(new int[]{7, 7, 7, 7}), 7));
+
+        ListUtils.printList(solution2.removeElements(ListUtils.arrayToList(new int[]{1, 2, 6, 3, 4, 5, 6}), 6));
+        ListUtils.printList(solution2.removeElements(ListUtils.arrayToList(new int[]{}), 1));
+        ListUtils.printList(solution2.removeElements(ListUtils.arrayToList(new int[]{7, 7, 7, 7}), 7));
+
+    }
+
+    class Solution {
+        public ListNode removeElements(ListNode head, int val) {
+            while (head != null && head.val == val)
+                head = head.next;
+            ListNode current = head;
+            while (current != null && current.next != null)
+                if (current.next.val == val)
+                    current.next = current.next.next;
+                else
+                    current = current.next;
+            return head;
+        }
+    }
+
+    class Solution2 {
+        public ListNode removeElements(ListNode head, int val) {
+            ListNode dummy = new ListNode();
+            dummy.next = head;
+            ListNode current = dummy;
+            while (current.next != null)
+                if (current.next.val == val)
+                    current.next = current.next.next;
+                else
+                    current = current.next;
+            return dummy.next;
+        }
+    }
+
+}
