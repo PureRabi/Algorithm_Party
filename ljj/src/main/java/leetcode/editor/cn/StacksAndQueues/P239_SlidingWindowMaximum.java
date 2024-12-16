cc5cbfae233d2c7ab3ae11040b4153219c392140
SolisAmicus <solisamicus@163.com>
2024-12-16 14:25:06 +0800

    docs: update README.md

---
+++ ljj/src/main/java/leetcode/editor/cn/StacksAndQueues/P239_SlidingWindowMaximum.java
@@ -0,0 +1,23 @@
+package leetcode.editor.cn.StacksAndQueues;
+
+import java.util.Arrays;
+
+/**
+ * 滑动窗口最大值
+ *
+ * @author solisamicus
+ * @date 2024-12-16 10:37:25
+ */
+public class P239_SlidingWindowMaximum {
+
+    public static void main(String[] args) {
+        Solution solution = new P239_SlidingWindowMaximum().new Solution();
+    }
+
+    class Solution {
+        public int[] maxSlidingWindow(int[] nums, int k) {
+            return null;
+        }
+    }
+
+}
